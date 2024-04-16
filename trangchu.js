6d491fd44a1f08d4fd82405ab2ff0202b8dd5267
Phongvu003 <nguyenphongvu003@gmail.com~>
2024-04-16 12:50:31 +0700

    first commit

---
+++ trangchu.js
@@ -0,0 +1,15 @@
+//Nhấn THêm Món hiển thị
+function nhan(){
+    alert('Thêm Món Thành Công');
+}
+
+//Hộp CHatbot
+/*Hàm Mở Form*/
+function moForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+/*Hàm Đóng Form*/
+function dongForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
